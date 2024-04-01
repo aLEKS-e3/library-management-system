09f465747fe607adef5d8d1e46613265f4c9bf5c
AntonSkyrda <mr.skyrda@gmail.com>
2024-04-02 00:02:08 +0300

    add permission IsAdminOrReadOnly to book model

--- books_service/views.py
+++ books_service/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from books_service.models import Book
 from books_service.serializers import BookSerializer
+from books_service.permissions import IsAdminOrReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly, )
