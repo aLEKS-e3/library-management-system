9f3bb9d505a13c4103cfac3aecf0d6db5299cf2f
kabust <tulyulyuk91@gmail.com>
2024-04-01 12:52:00 +0200

    removed redundant tests and added password length verification

--- user/tests/test_user.py
+++ user/tests/test_user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 from rest_framework.test import APIClient
 from django.test import TestCase
 
@@ -18,18 +19,19 @@ class UserApiTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        payload = {"email": "test@test.com", "password": "test_password"}
-
         self.user = get_user_model().objects.create_user(
-            email=payload["email"], password=payload["password"]
+            email="test@test.com", password="test_password"
         )
 
-        res = self.client.post(
-            path=reverse("user:token_obtain_pair"),
-            data=payload
+    def test_password_validation(self):
+        payload = {"email": "register@test.com", "password": "shrt"}
+        url = reverse("user:create")
+        res = self.client.post(path=url, data=payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            res.data.get("password"),
+            [ErrorDetail(string='Ensure this field has at least 5 characters.', code='min_length')]
         )
-        self.refresh = res.data.get("refresh")
-        self.access = res.data.get("access")
 
     def test_register_user(self):
         payload = {"email": "register@test.com", "password": "test_password"}
@@ -40,30 +42,6 @@ class UserApiTest(TestCase):
             res.data.get("email"),
             get_user_model().objects.last().email
         )
-
-    def test_get_token(self):
-        payload = {"email": "test@test.com", "password": "test_password"}
-        url = reverse("user:token_obtain_pair")
-        res = self.client.post(path=url, data=payload)
-
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertIn("refresh", res.data)
-        self.assertIn("access", res.data)
-
-    def test_refresh_token(self):
-        payload = {"refresh": self.refresh}
-        url = reverse("user:token_refresh")
-        res = self.client.post(path=url, data=payload)
-
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertIn("access", res.data)
-
-    def test_verify_token(self):
-        payload = {"token": self.access}
-        url = reverse("user:token_verify")
-        res = self.client.post(path=url, data=payload)
-
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_user_manage_access(self):
         self.client.force_authenticate(self.user)
