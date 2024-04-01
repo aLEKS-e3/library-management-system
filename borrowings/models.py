6093bca3657885f2c9cad6a57327de8a1979b30e
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-01 19:51:13 +0300

    add auto_now_add to borrow_date

--- borrowings/models.py
+++ borrowings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book_id = models.PositiveIntegerField()
