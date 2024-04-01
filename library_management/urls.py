c6e49ad085fdfbf0a0c8811d58e7dc546400af1c
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-01 18:56:39 +0300

    add borrowings endpoint

--- library_management/urls.py
+++ library_management/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
-    path("api/literature/", include("books_service.urls", namespace="books_service"))
+    path("api/literature/", include("books_service.urls", namespace="books_service")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings"))
 ]
