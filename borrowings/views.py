8976c9d6f56b6e38edca82ab483a06bef06b3374
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-02 11:23:54 +0300

    change Views due to change in models

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,14 +1,17 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingDetailSerializer, BorrowingListSerializer
+from borrowings.serializers import BorrowingListSerializer, BorrowingSerializer
 
 
-class BorrowingListView(generics.ListCreateAPIView):
+class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingListSerializer
+    serializer_class = BorrowingSerializer
 
+    def get_queryset(self):
+        return Borrowing.objects.filter(user=self.request.user)
 
-class BorrowingDetailView(generics.RetrieveAPIView):
-    queryset = Borrowing.objects.all()
-    serializer_class = BorrowingDetailSerializer
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BorrowingListSerializer
+        return self.serializer_class
