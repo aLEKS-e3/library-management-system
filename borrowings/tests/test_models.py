a02b7337a12e428a586364787b4b412280e004fd
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-02 16:34:23 +0300

    change tests due to change in models

--- borrowings/tests/test_models.py
+++ borrowings/tests/test_models.py
@@ -1,10 +1,27 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+
+from books_service.models import Book
 from borrowings.models import Borrowing
+from user.models import User
 
 
 class BorrowingModelTest(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="Test Cover",
+            inventory=20,
+            daily_fee=20.0
+
+        )
+        self.user = User.objects.create_user(
+            email="test@test.com",
+            password="test"
+        )
+
     def test_valid_borrowing(self):
         """
         Test that a Borrowing object with valid dates does not raise any validation errors.
@@ -13,8 +30,8 @@ class BorrowingModelTest(TestCase):
             borrow_date=timezone.now(),
             expected_return_date=timezone.now() + timezone.timedelta(days=7),
             actual_return_date=None,
-            book_id=1,
-            user_id=1
+            book=self.book,
+            user=self.user
         )
         # This should not raise any validation errors
         borrowing.full_clean()
@@ -27,8 +44,8 @@ class BorrowingModelTest(TestCase):
             borrow_date=timezone.now(),
             expected_return_date=timezone.now() - timezone.timedelta(days=7),
             actual_return_date=None,
-            book_id=1,
-            user_id=1
+            book=self.book,
+            user=self.user
         )
         with self.assertRaises(ValidationError):
             borrowing.full_clean()
@@ -41,8 +58,8 @@ class BorrowingModelTest(TestCase):
             borrow_date=timezone.now(),
             expected_return_date=timezone.now() + timezone.timedelta(days=7),
             actual_return_date=timezone.now() - timezone.timedelta(days=7),
-            book_id=1,
-            user_id=1
+            book=self.book,
+            user=self.user
         )
         with self.assertRaises(ValidationError):
             borrowing.full_clean()
@@ -55,7 +72,7 @@ class BorrowingModelTest(TestCase):
             borrow_date=timezone.now() - timezone.timedelta(days=7),
             expected_return_date=timezone.now(),
             actual_return_date=timezone.now(),
-            book_id=1,
-            user_id=1
+            book=self.book,
+            user=self.user
         )
         borrowing.full_clean()
