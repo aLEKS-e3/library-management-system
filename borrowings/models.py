031da23061e7e12602a2909fe6be789658bae661
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-02 11:22:08 +0300

    change models user_id -> user, book_id -> book

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,13 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
+
+from books_service.models import Book
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book_id = models.PositiveIntegerField()
-    user_id = models.PositiveIntegerField()
+    book = models.ForeignKey(
+        to=Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
 
     def clean(self):
         if self.borrow_date > self.expected_return_date:
