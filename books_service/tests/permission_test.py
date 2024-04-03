229e5b48b2d4ed0c303abd3918c758b05cc6dc88
AntonSkyrda <mr.skyrda@gmail.com>
2024-04-03 15:35:32 +0300

    delete constants

--- books_service/tests/permission_test.py
+++ books_service/tests/permission_test.py
@@ -6,63 +6,59 @@ from books_service.permissions import IsAdminOrReadOnly
 from books_service.views import BookViewSet
 
 
-LIST_URL = "/books/"
-RETRIEVE_URL = LIST_URL + "f'/books/{book.id}/'"
-
-
 class PermissionsTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.book = Book.objects.create(
-            title='Test Book',
-            author='Test Author',
-            cover='HARD',
+            title="Test Book",
+            author="Test Author",
+            cover="HARD",
             inventory=10,
             daily_fee=5.99)
         self.admin_user = get_user_model().objects.create_user(
-            email='admin@admin.com',
-            password='adminpass', is_staff=True
+            email="admin@admin.com",
+            password="adminpass", is_staff=True
         )
         self.normal_user = get_user_model().objects.create_user(
-            email='user@user.com',
-            password='userpass'
+            email="user@user.com",
+            password="userpass"
         )
 
     def test_read_only_for_unauthenticated_user(self):
-        request = self.factory.get('/books/')
+        request = self.factory.get("/books/")
         request.user = None
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
 
     def test_read_only_for_authenticated_user(self):
-        request = self.factory.get('/books/')
+        request = self.factory.get("/books/")
         request.user = self.normal_user
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
 
     def test_read_write_for_admin_user(self):
-        request = self.factory.post(LIST_URL)
+        request = self.factory.post("/books/")
         request.user = self.admin_user
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
 
     def test_read_only_for_unauthenticated_user_detail(self):
         book = self.book
-        request = self.factory.get(RETRIEVE_URL)
+        request = self.factory.get(f"/books/{book.id}/")
         request.user = None
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
 
     def test_read_only_for_authenticated_user_detail(self):
         book = self.book
-        request = self.factory.get(RETRIEVE_URL)
+        request = self.factory.get(f"/books/{book.id}/")
         request.user = self.normal_user
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
 
     def test_read_write_for_admin_user_detail(self):
         book = self.book
-        request = self.factory.post(RETRIEVE_URL)
+        request = self.factory.post(f"/books/{book.id}/")
         request.user = self.admin_user
         permission = IsAdminOrReadOnly()
         self.assertTrue(permission.has_permission(request, BookViewSet()))
