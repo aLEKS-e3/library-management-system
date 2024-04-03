5bfb0bbaadd11cf3a0c6825948ef1a0aa51bdaf5
Yevhen <evgeniy.playid@icloud.com>
2024-04-03 12:17:25 +0300

    Remove redundant IsAuthenticatedOrReadOnly

--- library_management/settings.py
+++ library_management/settings.py
@@ -128,9 +128,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ],
 }
 
 SIMPLE_JWT = {
